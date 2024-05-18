679babe712b2a4b0835c0366b7ffb175806fbf95
joseph <joseph_vv@hotmail.com>
2024-05-18 01:11:34 -0300

    responsive menu, new ver of tailwind

--- app/Components/Navigation.js
+++ app/Components/Navigation.js
@@ -1,35 +1,71 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Link from "next/link";
 
-const links = [
-  {
-    label: "INICIO",
-    route: "/",
-  },
-  {
-    label: "PRODUCTOS",
-    route: "/productos",
-  },
-  {
-    label: "NOSOTROS",
-    route: "/acerca",
-  },
-];
-
 function Navigation() {
+  const links = [
+    {
+      label: "INICIO",
+      route: "/",
+    },
+    {
+      label: "PRODUCTOS",
+      route: "/productos",
+    },
+    {
+      label: "NOSOTROS",
+      route: "/acerca",
+    },
+  ];
+
+  const [clicked, setClicked] = useState(false);
+
+  const toggleNav = () => {
+    setClicked(!clicked);
+  };
+
   return (
     <header>
-      <nav className=" flex w-full justify-center py-3">
-        <div className="flex items-center  gap-x-4 bg-white max-w-sm border border-black ">
-          <img className=" h-20 w-auto" src="logo.jpg" alt="LUBESAC" />
-          <div className=" text-xl font-medium text-black leading-5">
-            Lubricantes Especiales del Perú
+      <nav className="flex flex-col  md:flex-row w-full justify-center py-3">
+        <Link href="/">
+          <div className="flex items-center  gap-x-4 bg-white max-w-sm ">
+            <img className=" h-20 w-auto" src="logo.jpg" alt="LUBESAC" />
+            <div className=" text-xl max-w- font-medium text-black leading-5">
+              Lubricantes Especiales del Perú
+            </div>
           </div>
+        </Link>
+
+        <div class="md:hidden absolute right-6 top-32 z-30">
+          <button
+            onClick={toggleNav}
+            class="navbar-burger flex items-center text-black p-3"
+          >
+            <svg
+              class="block h-8 w-8 fill-current"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Mobile menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+            </svg>
+          </button>
         </div>
-        <ul className="flex m-5 items-center gap-x-10 border border-black ">
+
+        <ul
+          className={`${
+            clicked
+              ? "flex flex-col fixed size-full top-0  m-auto z-20 bg-white  "
+              : "hidden"
+          } md:flex m-5 items-center gap-x-10 px-12`}
+        >
           {links.map(({ label, route }) => (
-            <li key={route} className="px-4">
-              <Link className=" block h-8  hover:text-yellow-500" href={route}>
+            <li key={route}>
+              <Link
+                onClick={toggleNav}
+                className=" block h-8  hover:text-yellow-500"
+                href={route}
+              >
                 {label}
               </Link>
             </li>
--- app/page.js
+++ app/page.js
@@ -1,50 +1,69 @@
-import Image from 'next/image'
-import Navigation from './Components/Navigation'
-import Link from 'next/link'
+import Image from "next/image";
+import Navigation from "./Components/Navigation";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-
-      <div className='relative'>
-        <div className='flex justify-center w-full h-[735px] '>
-          <img  src=" bg1.jpg" alt="aceite" className='w-full object-cover' />
+      <div className="relative">
+        <div className="flex justify-center w-full h-[735px] ">
+          <img src=" bg1.jpg" alt="aceite" className="w-full object-cover" />
         </div>
 
-        <div className=' absolute top-1/4 left-1/4   h-1/2 w-1/4 '>
-          <p className='text-white text-4xl font-extrabold select-none '>ESPECIALISTAS EN ACEITES INDUSTRIALES  </p>
-          <div className=' inline-block  bg-slate-50 bg-opacity-60 text-center mt-3'>
-            <Link href="productos" className=' block p-4'>
+        <div className=" absolute top-1/4 left-1/4   h-1/2 w-1/4 ">
+          <h1 className="text-white text-4xl font-extrabold select-none ">
+            ESPECIALISTAS EN ACEITES INDUSTRIALES
+          </h1>
+          <div className=" inline-block  bg-slate-50 hover:bg-opacity-70  bg-opacity-60 text-center mt-3 rounded-md">
+            <Link href="productos" className=" block p-4">
               Conocer productos
             </Link>
           </div>
         </div>
       </div>
 
-      <div className='flex flex-col gap-3 m-auto pt-20 pb-20 w-3/4'>
-        <p className='text-3xl font-bold text-yellow-500'>Lubricantes especiales del Perú</p>
-        <h2 className=' font-bold'>COMBINAMOS EXPERIENCIA E INNOVACION</h2>
-        <p className=''>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sem odio, maximus at luctus a, ultrices a augue. Nulla nunc turpis, mattis eu lacus at, laoreet porta risus. Fusce tempus, purus at convallis tempus, dui ipsum tincidunt velit, quis tincidunt metus arcu ac tortor. Proin iaculis velit dolor, at facilisis neque varius nec. Pellentesque diam neque, ornare ut metus nec, pretium maximus dolor. </p>
+      <div className="flex flex-col gap-3 m-auto pt-20 pb-20 w-3/4">
+        <p className="text-3xl font-bold text-yellow-500">
+          Lubricantes especiales del Perú
+        </p>
+        <h2 className=" font-bold">COMBINAMOS EXPERIENCIA E INNOVACION</h2>
+        <p className="">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sem
+          odio, maximus at luctus a, ultrices a augue. Nulla nunc turpis, mattis
+          eu lacus at, laoreet porta risus. Fusce tempus, purus at convallis
+          tempus, dui ipsum tincidunt velit, quis tincidunt metus arcu ac
+          tortor. Proin iaculis velit dolor, at facilisis neque varius nec.
+          Pellentesque diam neque, ornare ut metus nec, pretium maximus dolor.
+        </p>
       </div>
 
-      <div className='relative h-96 w-full'>
-        <img src="bg2.jpg" className=' w-full h-full object-cover filter brightness-50'  alt="" />
-        <div className='flex flex-col gap-6 absolute top-24 left-28 z-100 text-white text-2xl'>
-          <p className='font-bold' >NUESTROS SERVICIOS</p>
-          <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sem odio, maximus at luctus a, ultrices a augue. Nulla nunc turpis, mattis eu lacus at, laoreet porta risus.</p>
+      <div className="relative h-96 w-full">
+        <img
+          src="bg2.jpg"
+          className=" w-full h-full object-cover filter brightness-50"
+          alt=""
+        />
+        <div className="flex flex-col gap-6 absolute top-24 left-28 z-100 text-white text-2xl">
+          <p className="font-bold">NUESTROS SERVICIOS</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sem
+            odio, maximus at luctus a, ultrices a augue. Nulla nunc turpis,
+            mattis eu lacus at, laoreet porta risus.
+          </p>
         </div>
       </div>
 
-      <div className=' m-16' >
-        <h2 className=' text-center font-bold'>MARCAS CON LAS QUE TRABAJAMOS</h2>
-        <div className='flex justify-center gap-16 mt-10'>
-          <img src="shell.png" alt=""/>
+      <div className=" m-16">
+        <h2 className=" text-center font-bold">
+          MARCAS CON LAS QUE TRABAJAMOS
+        </h2>
+        <div className="flex justify-center gap-16 mt-10">
+          <img src="shell.png" alt="" />
           <img src="shell.png" alt="" />
           <img src="shell.png" alt="" />
           <img src="shell.png" alt="" />
         </div>
       </div>
     </div>
-    
-  )
-} 
+  );
+}
