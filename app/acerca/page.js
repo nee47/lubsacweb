ceed3b434b7ee006bc3fa31fdf6711b994b11a52
joseph <joseph_vv@hotmail.com>
2024-05-18 18:09:00 -0300

    deploy eerror fix

--- app/acerca/page.js
+++ app/acerca/page.js
@@ -1,19 +1,16 @@
-
 function page() {
   return (
-    <div className='h-full w-90 '>
-        <p className=' w-6/12 m-auto border-2 rounded-sm mt-3 border-black p-3 text-2xl'>
-        Tenemos el agrado de dirigirnos a usted para hacerle llegar nuestros cordiales saludos y del
-mismo modo aprovechar esta oportunidad para presentarle a nuestra Empresa,
-“LUBRICANTES ESPECIALES DEL PERU SAC", con RUC: 20523985842
+    <div className="h-full w-90 ">
+      <p className=" w-6/12 m-auto border-2 rounded-sm mt-3 border-black p-3 text-2xl">
+        Tenemos el agrado de dirigirnos a usted para hacerle llegar nuestros
+        cordiales saludos y del mismo modo aprovechar esta oportunidad para
+        presentarle a nuestra Empresa, LUBRICANTES ESPECIALES DEL PERU SAC, con
+        RUC: 20523985842
+      </p>
 
-    </p>
-
-    <div>
-      Contacto  
+      <div>Contacto</div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default page
+export default page;
